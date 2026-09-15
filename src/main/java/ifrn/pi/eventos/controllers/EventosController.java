05808a7328807eac2251971372508076bed49326
JOÃO PEDRO DOS SANTOS OLIVEIRA <20231204010010@ifrn.local>
2026-09-15 13:55:35 -0300

    Apagando um evento e convidados da aplicação

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -80,4 +80,35 @@ public class EventosController {
 		cr.save(convidado);
 		return "redirect:/eventos/{idEvento}";
 	}
+	
+	@GetMapping("/{id}/remover")
+	public String apagarEvento(@PathVariable Long id) {
+		
+		Optional<Evento> opt = er.findById(id);
+		
+		if(!opt.isEmpty()) {
+			Evento evento = opt.get();
+			
+			List<Convidado> convidados = cr.findByEvento(evento);
+			
+			cr.deleteAll(convidados);
+			er.delete(evento);
+		}
+		
+		return "redirect:/eventos";
+	}
+	@GetMapping("/{id}/removerConvidado")
+	public String apagarConvidado(@PathVariable Long id) {
+		
+		Optional<Convidado> opt = cr.findById(id);
+		
+		if(!opt.isEmpty()) {
+			Convidado convidado = opt.get();
+			
+			
+			cr.delete(convidado);
+		}
+		
+		return "redirect:/eventos/{id}";
+	}
 }
